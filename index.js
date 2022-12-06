72f03a845984e09a937ae9f6796cc2804e0b2e75
savan <savantrada0@gmail.com>
2022-12-06 21:50:52 +0530

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
+app.use("/", useRote);
+
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -16,10 +18,6 @@ app.use(cors());
 
 app.use("/users", userRouter); // http://localhost:5000/users/signup
 app.use("/tour", tourRouter);
-app.get("/", (req, res) => {
-  res.send("Welcome to tour API");
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome to tour API");
 });
